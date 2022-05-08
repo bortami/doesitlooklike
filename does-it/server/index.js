8b18835df0334c2afda4e85d2f2040e3d2152ce2
Michelle Tabor <mtabor66@outlook.com>
2022-05-07 19:16:08 -0500

    added MongoDB

--- does-it/server/index.js
+++ does-it/server/index.js
@@ -1,14 +1,28 @@
 // server/index.js
 
 const express = require("express");
-const PORT = process.env.PORT || 3001;
 const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" 
+});
 
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
+
+const PORT = process.env.PORT || 3001;
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
 
 
 app.listen(PORT, () => {
+     // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
   console.log(`Server listening on ${PORT}`);
 });
